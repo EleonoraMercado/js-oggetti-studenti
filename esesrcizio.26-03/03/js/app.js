f9d8681f93e56a2cffb18352ec9fb80f40405b79
EleonoraMercado <mercadoeleonora91@gmail.com>
2024-03-27 15:18:34 +0100

    ADD: tr html string.

--- esesrcizio.26-03/03/js/app.js
+++ esesrcizio.26-03/03/js/app.js
@@ -37,24 +37,31 @@ const students = [
 ];  //array
  console.log(students)
 
+const tBodyElement = document.getElementById('table-body');
+console.log(tBodyElement);
 
 //per ogni studente
 for (i = 0; i < students.length; i++) {
-    let currentStudent = students[i]
-    console.log(currentStudent)
+    const currentStudent = students[i]
+    console.log(currentStudent);
     //recuperiamo nome
-    const theFirstName = currentStudent.firstName
+    const theFirstName = currentStudent.firstName;
     //recuperiamo cognome
-    const theLastName = currentStudent.lastName
+    const theLastName = currentStudent.lastName;
     //recuperiamo età
-    const theAge = currentStudent.age
+    const theAge = currentStudent.age;
 
-    console.log(theFirstName, theLastName, theAge)
+    console.log(theFirstName, theLastName, theAge);
     //creiamo il tag html per ogni tr:
-    //<tr>
-    //  <td>firstName</td>
-    //  <td>lastName</td>
-    //  <td>age</td>
-    //</tr>
+    const trHTMLString = `
+    <tr>
+      <td>${theFirstName}</td>
+      <td>${theLastName}</td>
+      <td>${theAge}</td>
+    </tr>
+    ` 
+console.log(trHTMLString);
     //aggiungiamo il tag nel tbody della tabella
-}
+    tBodyElement.innerHTML += trHTMLString; 
+    //promemoria:+= di modo che gli elementi non sis sovrascrivano ma che se ne aggiunga uno nuovo.
+};
